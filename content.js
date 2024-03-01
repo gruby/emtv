1f89388a18f17b91f2259ffe7d47e8f23511d4a7
Robert Begier <robert.begier@gmail.com>
2024-03-01 20:21:35 +0100

    Update content.js

--- content.js
+++ content.js
@@ -7,11 +7,11 @@ let powerWords = [
   },
 */
   {
-    "words": "How many of you hear from God? Pray? You hear "fast" - you drop everything; even when you are eating, you hear "fast", you just drop the food.",
+    "words": "How many of you hear from God? Pray? You hear 'fast' - you drop everything; even when you are eating, you hear 'fast', you just drop the food.",
     "source": "TB"
   },
   {
-    "words": "How can the Word join the Spirit? If the Spirit does not give the Word, you are speaking to yourself. If you say, "Be healed" without Spirit suggestion, you are talking to yourself. Your confession cannot give you possession, without the Spirit giving that Word.",
+    "words": "How can the Word join the Spirit? If the Spirit does not give the Word, you are speaking to yourself. If you say, 'Be healed' without Spirit suggestion, you are talking to yourself. Your confession cannot give you possession, without the Spirit giving that Word.",
     "source": "TB"
   },
   {
