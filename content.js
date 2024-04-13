65bdae7c6892846346a2334bce37ab170df9757d
Robert Begier <robert.begier@gmail.com>
2024-04-13 09:52:30 +0200

    Update content.js

--- content.js
+++ content.js
@@ -11,7 +11,7 @@ let powerWords = [
   },
   {
     "words": "We overcome obstacles because we give out freely.",
-    "source": ""
+    "source": "TB"
   },
   {
     "words": "I took care of my relationship with Jesus, He took care of my success.",
