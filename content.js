caca5b574458e12e10cdf09700c054790980f33e
Robert Begier <robert.begier@gmail.com>
2024-04-26 06:28:43 +0200

    Update content.js

--- content.js
+++ content.js
@@ -6,6 +6,10 @@ let powerWords = [
   },
 */
   {
+    "words": "Action must come from the heart.",
+    "source": "TB"
+  },
+  {
     "words": "And pray that your flight may not be in winter.",
     "source": "Mark 13:18 ?"
   },
