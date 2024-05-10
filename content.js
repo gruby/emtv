1f25793a89c43d4e37709c70825cf6ca817b7f1d
Robert Begier <robert.begier@gmail.com>
2024-05-10 20:42:59 +0200

    Update content.js

--- content.js
+++ content.js
@@ -6,6 +6,10 @@ let powerWords = [
   },
 */
   {
+    "words": "The controversy resulting in his resignation never slowed Smith down. In fact, it seemed to speed him up.",
+    "source": "GG Smith"
+  },
+  {
     "words":"He gives power to the weak, And to those who have no might He increases strength.",
     "source": "Is 40:29"
   },
