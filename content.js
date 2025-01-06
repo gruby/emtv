048351c2a57ba43ec26a082fb047e0e82b39d5a7
Robert Begier <robert.begier@gmail.com>
2025-01-06 14:37:00 +0100

    Update content.js

--- content.js
+++ content.js
@@ -6,6 +6,10 @@ let powerWords = [
   },
 */
   {
+    "words": "Crisis - it is not the time to relax and become discouraged. Dig deep and fight it through.",
+    "source": "TB"
+  },
+  {
     "words": "Both riches and honor come from You, And You reign over all. In Your hand is power and might; In Your hand it is to make great And to give strength to all.",
     "source": "1 Chr 29:12"
   },
